3b2ae65978a5f7f13c0daaf3f1aeabfc51f22228
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-26 20:40:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 def nyc_pigeon_organizer (data)
   final = {}
 
-  data.each do |first_level, all_other|
+  data.each { |first_level, all_other|
     all_other.each do |category, array|
-      array.each do |name|
+      array.each { |name|
         final[name] = {:color => [], :gender => [], :lives => []}
-      end 
-    end 
+      } 
+  }
   end 
   x = final.keys
   data[:color].each do |bird_color, name|
